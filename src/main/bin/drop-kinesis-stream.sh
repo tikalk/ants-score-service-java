f14e52e1f3858135ff380db303b9162bd17f6b47
yanai <yanai@tikalk.com>
2018-02-12 13:53:46 +0200

    serverless

--- src/main/bin/drop-kinesis-stream.sh
+++ src/main/bin/drop-kinesis-stream.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
+export uuid=`aws lambda list-event-source-mappings --function-name Ants-Smashing-ProcessKinesisHitTrialEvents | jq .EventSourceMappings[0].UUID | sed s/\"//g`
+echo event_uuid is $event_uuid
+if [ ! -z "$event_uuid" ]
+then
+    echo "Removing event mapping $event_uuid"
+    aws lambda delete-event-source-mapping --uuid $event_uuid
+fi
+
+export uuid=`aws lambda list-event-source-mappings --function-name Ants-Smashing-ProcessKinesisHitTrialEvents | jq .EventSourceMappings[0].UUID | sed s/\"//g`
+echo event_uuid is $event_uuid
+if [ ! -z "$event_uuid" ]
+then
+    echo "Removing event mapping $event_uuid"
+    aws lambda delete-event-source-mapping --uuid $event_uuid
+fi
+
 aws kinesis delete-stream --stream-name Ants-Smashing-HitTrials
