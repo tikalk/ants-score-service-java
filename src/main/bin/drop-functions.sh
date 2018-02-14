59907bb46a6695a14b7e71c77cbbec8f74bfdfc0
yanai <yanai@tikalk.com>
2018-02-14 11:44:01 +0200

    serverless

--- src/main/bin/drop-functions.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-aws lambda delete-function --function-name ants-score-service-java-dev-kinesisHitTrialEventsJava
-aws lambda delete-function --function-name ants-score-service-java-dev-postHitTrialJava
-aws lambda delete-function --function-name scores-service-node-dev-getPlayersScores
-aws lambda delete-function --function-name scores-service-node-dev-getTeamsScores
