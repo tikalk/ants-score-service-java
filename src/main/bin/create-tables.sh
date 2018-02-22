34ba05afac3f9c536ec7b4d8a042442aefa98c93
yanai <yanai@tikalk.com>
2018-02-22 17:39:54 +0200

    kinesis

--- src/main/bin/create-tables.sh
+++ src/main/bin/create-tables.sh
@@ -3,9 +3,7 @@
 aws dynamodb create-table --table-name Ants-Smashing-PlayersScore \
 --attribute-definitions AttributeName=playerId,AttributeType=N AttributeName=score,AttributeType=N AttributeName=gameId,AttributeType=N \
 --key-schema AttributeName=playerId,KeyType=HASH --provisioned-throughput ReadCapacityUnits=10,WriteCapacityUnits=10 \
---global-secondary-indexes 'IndexName=gameId_score_idx,KeySchema=[{AttributeName=gameId,KeyType=HASH},{AttributeName=score,KeyType=RANGE}],Projection={ProjectionType=ALL},ProvisionedThroughput={ReadCapacityUnits=10,WriteCapacityUnits=10}' \
---stream-specification StreamEnabled=true,StreamViewType=NEW_IMAGE
-
+--global-secondary-indexes 'IndexName=gameId_score_idx,KeySchema=[{AttributeName=gameId,KeyType=HASH},{AttributeName=score,KeyType=RANGE}],Projection={ProjectionType=ALL},ProvisionedThroughput={ReadCapacityUnits=10,WriteCapacityUnits=10}'
 
 aws dynamodb create-table --table-name Ants-Smashing-TeamsScore \
 --attribute-definitions AttributeName=teamId,AttributeType=N AttributeName=score,AttributeType=N AttributeName=gameId,AttributeType=N AttributeName=updateDate,AttributeType=N AttributeName=updateTime,AttributeType=N \
@@ -13,8 +11,7 @@ aws dynamodb create-table --table-name Ants-Smashing-TeamsScore \
 --global-secondary-indexes '[
        { "IndexName":"gameId_score_idx", "KeySchema":[ { "AttributeName":"gameId", "KeyType":"HASH" }, { "AttributeName":"score", "KeyType":"RANGE" } ], "Projection":{ "ProjectionType":"ALL" }, "ProvisionedThroughput":{ "ReadCapacityUnits":10, "WriteCapacityUnits":10 } },
        { "IndexName":"date_time_idx", "KeySchema":[ { "AttributeName":"updateDate", "KeyType":"HASH" }, { "AttributeName":"updateTime", "KeyType":"RANGE" } ], "Projection":{ "ProjectionType":"ALL" }, "ProvisionedThroughput":{ "ReadCapacityUnits":10, "WriteCapacityUnits":10 } }
-]' \
---stream-specification StreamEnabled=true,StreamViewType=NEW_IMAGE
+]'
 
 aws dynamodb create-table --table-name Ants-Smashing-smashedAnts \
 --attribute-definitions AttributeName=antId,AttributeType=S \
